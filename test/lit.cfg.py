cec35836808f657bf3816fa4c77ce59f973ba202
Alexey Sachkov <alexey.sachkov@intel.com>
2018-12-06 14:33:48 +0300

    Test end-to-end tests

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -16,7 +16,7 @@ config.name = 'LLVM_SPIRV'
 config.test_format = lit.formats.ShTest(True)
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = ['.ll', '.spt']
+config.suffixes = ['.ll', '.spt', '.cl']
 
 # excludes: A list of directories  and fles to exclude from the testsuite.
 config.excludes = ['CMakeLists.txt']
